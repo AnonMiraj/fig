581594dbf870c1d06bb037a86bc9506d84d88815
anonmiraj <nabilmalek48@gmail.com>
2024-04-02 06:05:24 +0200

    alpha channel

--- src/rgb.f90
+++ src/rgb.f90
@@ -1,29 +1,34 @@
+
 module fig_rgb
     implicit none
 
     type :: RGB
         sequence
-        integer :: red
-        integer :: green
-        integer :: blue
+        integer :: r
+        integer :: g
+        integer :: b
+        integer :: a
     end type RGB
 
-    type(RGB) :: RED = RGB(255, 0, 0)
-    type(RGB) :: GREEN = RGB(0, 255, 0)
-    type(RGB) :: BLUE = RGB(0, 0, 255)
-    type(RGB) :: WHITE = RGB(255, 255, 255)
-    type(RGB) :: BLACK = RGB(0, 0, 0)
-    type(RGB) :: YELLOW = RGB(255, 255, 0)
-    type(RGB) :: CYAN = RGB(0, 255, 255)
-    type(RGB) :: MAGENTA = RGB(255, 0, 255)
-    type(RGB) :: PINK = RGB(255, 192, 203)
+    type(RGB) :: RED = RGB(255, 0, 0, 255) 
+    type(RGB) :: GREEN = RGB(0, 255, 0, 255)
+    type(RGB) :: BLUE = RGB(0, 0, 255, 255)
+    type(RGB) :: WHITE = RGB(255, 255, 255, 255)
+    type(RGB) :: BLACK = RGB(0, 0, 0, 255)
+    type(RGB) :: YELLOW = RGB(255, 255, 0, 255)
+    type(RGB) :: CYAN = RGB(0, 255, 255, 255)
+    type(RGB) :: MAGENTA = RGB(255, 0, 255, 255)
+    type(RGB) :: PINK = RGB(255, 192, 203, 255)
 
 contains
 
     function rgb_to_int(color) result(rgb_int)
         type(RGB), intent(in) :: color
         integer :: rgb_int
-        rgb_int = color%blue * 65536 + color%green * 256 + color%red
+
+        rgb_int = ior(ishft(color%a, 24), ior(ishft(color%r, 16), ior(ishft(color%g, 8), color%b)))
+
     end function rgb_to_int
 
 end module fig_rgb
+
