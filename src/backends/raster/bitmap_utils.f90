b50d0673361915e73dec070bd295cf65d8d34bdf
anonmiraj <nabilmalek48@gmail.com>
2024-06-03 21:09:39 +0300

    move draw_line to utils

--- src/backends/raster/bitmap_utils.f90
+++ src/backends/raster/bitmap_utils.f90
@@ -38,6 +38,42 @@ contains
 
     end subroutine fill_rect
 
+    subroutine draw_line(canva, pixels, x1,y1,x2,y2,color)
+        class(base_canvas), intent(inout) :: canva
+        integer(pixel), dimension(0:,0:), intent(inout):: pixels
+        integer(pixel), intent(in) :: color
+
+        real, intent(in) :: x1, y1, x2, y2
+        integer :: dx, dy, x, y
+        integer :: sx, sy, err, e2
+        
+        dx = abs(int(x2 - x1))
+        dy = abs(int(y2 - y1))
+        
+        sx = sign(1,int(x2 - x1))
+        sy = sign(1,int(y2 - y1))
+        
+        err = dx - dy
+        
+        x = x1
+        y = y1
+        
+        do while ((x /= int(x2) .or. y /= int(y2)) .and. &
+                  (x >= 0 .and. x < int(canva%width)) .and. &
+                  (y >= 0 .and. y < int(canva%height)))
+            pixels(x, y) = color 
+            e2 = 2 * err
+            if (e2 > -dy) then
+                err = err - dy
+                x = x + sx
+            end if
+            if (e2 < dx) then
+                err = err + dx
+                y = y + sy
+            end if
+        end do
+    end subroutine draw_line
+ 
 
 
     
