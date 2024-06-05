8a00662a0d78d9104baa1dc7d6472dc44370a32f
anonmiraj <nabilmalek48@gmail.com>
2024-06-06 01:46:37 +0300

    improve triangle filling

--- src/backends/raster/bitmap_utils.f90
+++ src/backends/raster/bitmap_utils.f90
@@ -76,12 +76,9 @@ contains
         end do
     end subroutine draw_line
  
-
-
     subroutine fill_triangle(canva, pixels, x1, y1, x2, y2, x3, y3, color)
-        !! TODO needs to organize this a bit
         class(base_canvas), intent(inout) :: canva
-        integer, dimension(0:,0:), intent(inout) :: pixels
+        integer, dimension(:,:), intent(inout) :: pixels
         integer, intent(in) :: x1, y1, x2, y2, x3, y3, color
         integer :: p1(2), p2(2), p3(2)
         integer :: x, y
@@ -99,56 +96,48 @@ contains
         dy12 = p2(2) - p1(2)
         dx13 = p3(1) - p1(1)
         dy13 = p3(2) - p1(2)
-
-        ! Fill the top part of the triangle
-        do y = max(p1(2), 0), min(p2(2), int(canva%height) - 1), 1
-            if (dy12 /= 0) then
-                x_start = (y - p1(2)) * dx12 / dy12 + p1(1)
-            else
-                x_start = p1(1)
-            end if
-
-            if (dy13 /= 0) then
-                x_end = (y - p1(2)) * dx13 / dy13 + p1(1)
-            else
-                x_end = p1(1)
-            end if
-
-            if (x_start>x_end) call swap_integers(x_start,x_end)
-            x_start = max(x_start, 0)
-            x_end = min(x_end, int(canva%width) - 1)
-
-            do x = x_start, x_end, 1
-                pixels(x,y)= blend_color(pixels(x,y),color)
-            end do
-        end do
-
         dx32 = p2(1) - p3(1)
         dy32 = p2(2) - p3(2)
         dx31 = p1(1) - p3(1)
         dy31 = p1(2) - p3(2)
 
-        ! Fill the bottom part of the triangle   
-        do y = max(p2(2), 0), min(p3(2), int(canva%height) - 1), 1
-            if (dy32 /= 0) then
-                x_start = (y - p3(2)) * dx32 / dy32 + p3(1)
+        ! Fill the triangle   
+        do y = max(p1(2), 0), min(p3(2), int(canva%height) - 1), 1
+            if (y <= p2(2)) then
+                ! Top part of triangle
+                if (dy12 /= 0) then
+                    x_start = (y - p1(2)) * dx12 / dy12 + p1(1)
+                else
+                    x_start = p1(1)
+                end if
+
+                if (dy13 /= 0) then
+                    x_end = (y - p1(2)) * dx13 / dy13 + p1(1)
+                else
+                    x_end = p1(1)
+                end if
             else
-                x_start = p3(1)
+                ! Bottom part of triangle
+                if (dy32 /= 0) then
+                    x_start = (y - p3(2)) * dx32 / dy32 + p3(1)
+                else
+                    x_start = p3(1)
+                end if
+
+                if (dy31 /= 0) then
+                    x_end = (y - p3(2)) * dx31 / dy31 + p3(1)
+                else
+                    x_end = p3(1)
+                end if
             end if
 
-            if (dy31 /= 0) then
-                x_end = (y - p3(2)) * dx31 / dy31 + p3(1)
-            else
-                x_end = p3(1)
-            end if
-
-            if (x_start>x_end) call swap_integers(x_start,x_end)
+            if (x_start > x_end) call swap_integers(x_start, x_end)
             x_start = max(x_start, 0)
             x_end = min(x_end, int(canva%width) - 1)
+
             do x = x_start, x_end, 1
-                pixels(x,y)= blend_color(pixels(x,y),color)
+                call blend_pixel(pixels,x,y,color)
             end do
-                
         end do
     end subroutine fill_triangle
 
